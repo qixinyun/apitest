a04f3e5fea476942bad3ebc908db1e3abaa096eb
chloroplast <41893204@qq.com>
2016-11-25 13:41:19 +0800

    接口测试文件

--- member.api.qixinyun.com/UserUtilsTest.php
+++ member.api.qixinyun.com/UserUtilsTest.php
@@ -4,74 +4,98 @@ namespace User;
 require '../vendor/autoload.php';
 use GuzzleHttp;
 
-$client = new GuzzleHttp\Client();
+// $client = new GuzzleHttp\Client(['base_uri' => 'http://139.224.65.141:8001/']);
+$client = new GuzzleHttp\Client(['base_uri' => 'http://127.0.0.1/']);
+
 //get 
-/*$response = $client->request(
-                'GET',
-                'http://139.224.65.141:8001/users',
-                [
-                'haders'=>['Content-Type' => 'application/vnd.api+json'],
-                ]
-            );*/
+// $response = $client->request('GET', 'users/1',['haders'=>['Content-' => 'application/vnd.api+json']]);
+// $response = $client->request('GET', 'users/1,5',['haders'=>['Content-' => 'application/vnd.api+json']]);
 
-/*$data = array("data"=>array("type"=>"users",
-                            "attributes"=>array("cellPhone"=>"12345678933",
-                                                "password"=>"123456"
-                                               )
-		                   )
-		      );*/
-
-//signUp
-/*$response = $client->request(
-                'POST',
-                'http://127.0.0.1/users',
-                [
-                'haders'=>['Content-Type' => 'application/vnd.api+json'],
-                'json' => $data
-                ]
-            );*/
-
-//restPassword
-/*$response = $client->request(
-                'PUT',
-                'http://127.0.0.1/restPassword',
-                [
-                'haders'=>['Content-Type' => 'application/vnd.api+json'],
-                'json' => $data
-                ]
-            );*/
+//signUp -- 开始
+// $data = array("data"=>array("type"=>"users",
+//                             "attributes"=>array("cellPhone"=>"15202939431",
+//                                                 "password"=>"123456"
+//                                                )
+//                            )
+//               );
+// $response = $client->request(
+//                 'POST',
+//                 'users',
+//                 [
+//                 'haders'=>['Content-Type' => 'application/vnd.api+json'],
+//                 'json' => $data
+//                 ]
+//             );
+//signUp -- 结束
 
 //signIn
-/*$response = $client->request(
-                'POST',
-                'http://127.0.0.1/users/signIn',
-                [
-                'haders'=>['Content-Type' => 'application/vnd.api+json'],
-                'json' => $data
-                ]
-            );*/
+// $data = array("data"=>array("type"=>"users",
+//                             "attributes"=>array("cellPhone"=>"15202939431",
+//                                                 "password"=>"123456"
+//                                                )
+//                            )
+//               );
+// $response = $client->request(
+//                 'POST',
+//                 'users/signIn',
+//                 [
+//                 'haders'=>['Content-Type' => 'application/vnd.api+json'],
+//                 'json' => $data
+//                 ]
+//             );
+
+
+//restPassword
+// $data = array("data"=>array("type"=>"users",
+//                             "attributes"=>array("cellPhone"=>"15202939431",
+//                                                 "password"=>"1111"
+//                                                )
+//                            )
+//               );
+// $response = $client->request(
+//                 'PUT',
+//                 'restPassword',
+//                 [
+//                 'haders'=>['Content-Type' => 'application/vnd.api+json'],
+//                 'json' => $data
+//                 ]
+//             );
 
 //updatePassword
+// $data = array("data"=>array("type"=>"users",
+//                             "attributes"=>array("oldPassword"=>"1111",
+//                                                 "password"=>"123456"
+//                                                )
+//                            )
+//               );
+// $response = $client->request(
+//                 'PUT',
+//                 'users/6/updatePassword',
+//                 [
+//                 'haders'=>['Content-Type' => 'application/vnd.api+json'],
+//                 'json' => $data
+//                 ]
+//             );
+
+//编辑用户信息
 $data = array("data"=>array("type"=>"users",
-                            "attributes"=>array("oldPassword"=>"111111",
-                                                "password"=>"123456"
+                            "attributes"=>array("nickName"=>"nickName111"
                                                )
                            )
               );
 $response = $client->request(
                 'PUT',
-                'http://127.0.0.1/users/2/updatePassword',
+                'users/6',
                 [
                 'haders'=>['Content-Type' => 'application/vnd.api+json'],
                 'json' => $data
                 ]
             );
 
-
 $status = $response->getStatusCode();
 $body = $response->getBody();
 $contents = $body->getContents();
 
 echo "<pre>";
-var_dump($contents);
+print_r($contents);
 
