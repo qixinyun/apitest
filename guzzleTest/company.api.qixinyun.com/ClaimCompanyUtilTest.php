78735089022f40683eea240a0d3e36cf1e749195
leonNJ <1119679388@qq.com>
2016-11-29 19:14:24 +0800

    认领企业、申请企业、股东接口测试

---
+++ guzzleTest/company.api.qixinyun.com/ClaimCompanyUtilTest.php
@@ -0,0 +1,57 @@
+<?php
+namespace Company;
+
+require '../vendor/autoload.php';
+use GuzzleHttp;
+
+$client = new GuzzleHttp\Client();
+//get 
+/*$response = $client->request(
+                'GET',
+                'http://127.0.0.1/claimCompanyForms',
+                [
+                'haders'=>['Content-Type' => 'application/vnd.api+json'],
+                ]
+            );*/
+//,
+$data = array("data"=>array("type"=>"claimCompanyForms",
+                                    "attributes"=>array( "name"=>"企业名称12",
+                                                         "certificate"=>array('certificate111','certificate2','certificate3'),
+                                                         "province"=>"20",
+                                                         "city"=>"30",
+                                                         "region"=>"40",
+                                                         "uid"=>"1",
+                                                         "principalRealName"=>"法人名字",
+                                                         "principalIdentifyCardNumber"=>"123456798962",
+                                                         "principalCertificate"=>"xxxxxxxxxxx",
+                                                         "contractPhone"=>"18840287763"
+                                                         )
+                                       )
+                          );
+
+//"companyId"=>"1",
+/*$response = $client->request(
+                'POST',
+                'http://127.0.0.1/claimCompanyForms',
+                [
+                'haders'=>['Content-Type' => 'application/vnd.api+json'],
+                'json' => $data
+                ]
+            );*/
+$response = $client->request(
+                'PUT',
+                'http://127.0.0.1/claimCompanyForms/1',
+                [
+                'haders'=>['Content-Type' => 'application/vnd.api+json'],
+                'json' => $data
+                ]
+            );
+
+
+$status = $response->getStatusCode();
+$body = $response->getBody();
+$contents = $body->getContents();
+
+echo "<pre>";
+var_dump($contents);
+
--- guzzleTest/company.api.qixinyun.com/ShareholderUtilTest.php
+++ guzzleTest/company.api.qixinyun.com/ShareholderUtilTest.php
@@ -1,25 +1,25 @@
 <?php
-namespace User;
+namespace Company;
 
 require '../vendor/autoload.php';
 use GuzzleHttp;
 
 $client = new GuzzleHttp\Client();
-//get 
-/*$response = $client->request(
+//get   ?filter[name]=4
+$response = $client->request(
                 'GET',
-                'http://139.224.65.141:8001/users',
+                'http://127.0.0.1/shareholders',
                 [
                 'haders'=>['Content-Type' => 'application/vnd.api+json'],
                 ]
-            );*/
-//,
-$data = array("data"=>array("type"=>"shareholders",
+            );
+
+/*$data = array("data"=>array("type"=>"shareholders",
                                     "attributes"=>array( "companyId"=>"1",
-                                    					 "name"=>"股东名称",
+                                    					 "name"=>"股东名称12233",
                                                          "category"=>"1",
                                                          "certificateType"=>"1",
-                                                         "certificateNumber"=>"1234567896321456",
+                                                         "certificateNumber"=>"1234567896321465",
                                                          "subscribedContributionMethod"=>"1",
                                                          "subscribedContribution"=>"1000",
                                                          "subscribedContributionTime"=>"20161125",
@@ -28,38 +28,19 @@ $data = array("data"=>array("type"=>"shareholders",
                                                          "actualContributionTime"=>"20161125"
                                                          )
                                        )
-                          );
+                          );*/
 
-$response = $client->request(
+/*$response = $client->request(
                 'POST',
                 'http://127.0.0.1/shareholders',
                 [
                 'haders'=>['Content-Type' => 'application/vnd.api+json'],
                 'json' => $data
                 ]
-            );
+            );*/
 /*$response = $client->request(
                 'PUT',
-                'http://127.0.0.1/restPassword',
-                [
-                'haders'=>['Content-Type' => 'application/vnd.api+json'],
-                'json' => $data
-                ]
-            );*/
-
-/*$response = $client->request(
-                'POST',
-                'http://127.0.0.1/users/signIn',
-                [
-                'haders'=>['Content-Type' => 'application/vnd.api+json'],
-                'json' => $data
-                ]
-            );*/
-
-//"oldPassword"=>"111111"
-/*$response = $client->request(
-                'PUT',
-                'http://127.0.0.1/users/2/updatePassword',
+                'http://127.0.0.1/shareholders/11',
                 [
                 'haders'=>['Content-Type' => 'application/vnd.api+json'],
                 'json' => $data
