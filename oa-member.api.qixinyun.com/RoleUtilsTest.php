ab6445b1b0cf3a6d302211fd710aed444882889b
leonNJ <1119679388@qq.com>
2016-12-28 00:12:11 +0800

    修改角色接口测试文件

--- oa-member.api.qixinyun.com/RoleUtilsTest.php
+++ oa-member.api.qixinyun.com/RoleUtilsTest.php
@@ -4,19 +4,21 @@ namespace User;
 require '../vendor/autoload.php';
 use GuzzleHttp;
 
-$client = new GuzzleHttp\Client(['base_uri' => 'http://139.224.65.136:8002/']);
-// $client = new GuzzleHttp\Client(['base_uri' => 'http://127.0.0.1/']);
+// $client = new GuzzleHttp\Client(['base_uri' => 'http://139.224.65.136:8002/']);
+ $client = new GuzzleHttp\Client(['base_uri' => 'http://127.0.0.1/']);
 
 //get 
-// $response = $client->request('GET', 'roles',['haders'=>['Content-' => 'application/vnd.api+json']]);
+$response = $client->request('GET', 'roles/49',['haders'=>['Content-' => 'application/vnd.api+json']]);
 // $response = $client->request('GET', 'roles/1,2',['haders'=>['Content-' => 'application/vnd.api+json']]);
 // $response = $client->request('GET', 'roles?filter[userGroupId]=1&filter[roleStatus]=0&filter[scope]=2',['haders'=>['Content-' => 'application/vnd.api+json']]);
 
 //add -- 开始
-$data = array("data"=>array("type"=>"roles",
-                            "attributes"=>array("name"=>"roleName2",
+/*$data = array("data"=>array("type"=>"roles",
+                            "attributes"=>array("name"=>"roleName2dd",
                                                 "userGroupId"=>16,
-                                                "purview"=>array(1,2,3),
+                                                "purviewResultList"=>array(
+                                                            '101001'=>['add'],'101002'=>['add','edit'],'101003'=>['edit']
+                                                        ),
                                                 "scope"=>2
                                                )
                            )
@@ -28,24 +30,27 @@ $response = $client->request(
                 'haders'=>['Content-Type' => 'application/vnd.api+json'],
                 'json' => $data
                 ]
-            );
+            );*/
+//add -- 结束
 
 //edit -- 开始
-// $data = array("data"=>array("type"=>"roles",
-//                             "attributes"=>array(
-//                                                 "name"=>"name3333",
-//                                                 "purview"=>array(1,2,3)
-//                                                )
-//                            )
-//               );
-// $response = $client->request(
-//                 'PUT',
-//                 'roles/3',
-//                 [
-//                 'haders'=>['Content-Type' => 'application/vnd.api+json'],
-//                 'json' => $data
-//                 ]
-//             );
+/*$data = array("data"=>array("type"=>"roles",
+                            "attributes"=>array(
+                                                "name"=>"name3333",
+                                                "purviewResultList"=>array(
+                                                            '101001'=>[],'101002'=>['add','edit'],'101003'=>[]
+                                                        )
+                                               )
+                           )
+              );
+$response = $client->request(
+                'PUT',
+                'roles/49',
+                [
+                'haders'=>['Content-Type' => 'application/vnd.api+json'],
+                'json' => $data
+                ]
+            );*/
 
 //enabled --开始
 /*$response = $client->request(
@@ -83,4 +88,5 @@ $contents = $body->getContents();
 //edit -- 结束
 
 echo "<pre>";
+print_r($status);
 print_r($contents);
