5d516654b459e3c32c8eef3f02db4f394b66fa87
chloroplast <41893204@qq.com>
2016-12-27 18:39:26 +0800

    修改权限

--- purview.api.qixinyun.com/RolePurviewUtilTest.php
+++ purview.api.qixinyun.com/RolePurviewUtilTest.php
@@ -3,7 +3,7 @@ require '../vendor/autoload.php';
 
 $client = new GuzzleHttp\Client(['base_uri' => 'http://127.0.0.1/']);
 // $response = $client->request('GET', 'rolePurviews/1',['haders'=>['Content-' => 'application/vnd.api+json']]);
-
+$response = $client->request('GET', 'rolePurviews',['haders'=>['Content-' => 'application/vnd.api+json']]);
 //add
 // $data = array("data"=>array("type"=>"rolePurviews",
 //                                     "attributes"=>array("platformId"=>1,
@@ -25,23 +25,23 @@ $client = new GuzzleHttp\Client(['base_uri' => 'http://127.0.0.1/']);
 //             );
 
 //edit
-$data = array("data"=>array("type"=>"rolePurviews",
-                                    "attributes"=>array(
-                                    					"purviewResultList"=>array(
-                                    						'code1'=>array('value1'),'code2'=>'value1','code3'=>'value2','code4'=>15
-                                    					)
-                                                    )
-                                       )
-                          );
+// $data = array("data"=>array("type"=>"rolePurviews",
+//                                     "attributes"=>array(
+//                                     					"purviewResultList"=>array(
+//                                     						'code1'=>array('value1'),'code2'=>'value1','code3'=>'value2','code4'=>15
+//                                     					)
+//                                                     )
+//                                        )
+//                           );
 
-$response = $client->request(
-                'PUT',
-                'rolePurviews/1',
-                [
-                'haders'=>['Content-Type' => 'application/vnd.api+json'],
-                'json' => $data
-                ]
-            );
+// $response = $client->request(
+//                 'PUT',
+//                 'rolePurviews/platform/1/role/2',
+//                 [
+//                 'haders'=>['Content-Type' => 'application/vnd.api+json'],
+//                 'json' => $data
+//                 ]
+//             );
 
 $status = $response->getStatusCode();
 $body = $response->getBody();
