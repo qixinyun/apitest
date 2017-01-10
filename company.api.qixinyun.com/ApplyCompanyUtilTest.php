f3d7a6bb8fbb64ad64f5d1eabed72c42a2ffef8c
leonNJ <1119679388@qq.com>
2017-01-10 18:14:50 +0800

    修改企业服务测试文件

--- company.api.qixinyun.com/ApplyCompanyUtilTest.php
+++ company.api.qixinyun.com/ApplyCompanyUtilTest.php
@@ -4,35 +4,42 @@ namespace Company;
 require '../vendor/autoload.php';
 use GuzzleHttp;
 
-$client = new GuzzleHttp\Client();
-//get 
-$response = $client->request(
-                'GET',
-                'http://127.0.0.1/applyCompanyForms?filter[uid]=3',
-                [
-                'haders'=>['Content-Type' => 'application/vnd.api+json'],
-                ]
-            );
+//$client = new GuzzleHttp\Client(['base_uri' => 'http://139.224.65.141:8003/']);
+$client = new GuzzleHttp\Client(['base_uri' => 'http://127.0.0.1/']);
+
+//get  filter[name]  filter[principalRealName] 
+//sort=-id  sort=-name  sort=-principalRealName
+ $response = $client->request('GET', 'applyCompanyForms?sort=-principalRealName',['haders'=>['Content-' => 'application/vnd.api+json']]);
+// $response = $client->request('GET', 'applyCompanyForms/1,2',['haders'=>['Content-' => 'application/vnd.api+json']]);
+// $response = $client->request('GET', 'applyCompanyForms?filter[name]=555&filter[principalRealName]=ni',['haders'=>['Content-' => 'application/vnd.api+json']]);
 
 
 //add --start
 /*$data = array("data"=>array("type"=>"applyCompanyForms",
-                                    "attributes"=>array( "name"=>"企业名称555",
+                                    "attributes"=>array( "name"=>"xian555",
+                                                         "registrationNumber"=>"xxxxxxxxxx",
+                                                         "unifiedSocialCreditCode"=>"xxxxxxxxxx",
                                                          "certificate"=>array('certificate11','certificate22','certificate33'),
                                                          "province"=>20,
                                                          "city"=>30,
                                                          "region"=>40,
                                                          "uid"=>1,
-                                                         "principalRealName"=>"法人名字",
+                                                         "principalRealName"=>"nijing",
                                                          "principalIdentifyCardNumber"=>"123456798962",
-                                                         "principalCertificate"=>"xxxxxxxxxxx",
-                                                         "contractPhone"=>"18840287763"
+                                                         "principalIdentifyCardFrontPhoto"=>"11xxxxxxxxxx",
+                                                         "principalIdentifyCardBackPhoto"=>"22xxxxxxxxxx",
+                                                         "principalIdentifyCardHandHeldPhoto"=>"33xxxxxxxxxx",
+                                                         "principalBirthday"=>"2010-10-10",
+                                                         "principalBirthdayCategory"=>2,
+                                                         "contactName"=>"nijinghello",
+                                                         "contactDuties"=>"程序员",
+                                                         "contactPhone"=>"18840287763"
                                                          )
                                        )
                           );
 $response = $client->request(
                 'POST',
-                'http://127.0.0.1/applyCompanyForms',
+                'applyCompanyForms',
                 [
                 'haders'=>['Content-Type' => 'application/vnd.api+json'],
                 'json' => $data
@@ -43,21 +50,29 @@ $response = $client->request(
 //edit --start
 /*$data = array("data"=>array("type"=>"applyCompanyForms",
                                     "attributes"=>array( "name"=>"企业名称555",
-                                                         "certificate"=>array('certificate11','certificate22','certificate33'),
+                                                         "registrationNumber"=>"xxxxxxxxxx",
+                                                         "unifiedSocialCreditCode"=>"xxxxxxxxxx",
+                                                         "certificate"=>array('certificate1','certificate2','certificate3'),
                                                          "province"=>20,
                                                          "city"=>30,
                                                          "region"=>40,
                                                          "uid"=>1,
                                                          "principalRealName"=>"法人名字",
                                                          "principalIdentifyCardNumber"=>"123456798962",
-                                                         "principalCertificate"=>"xxxxxxxxxxx",
-                                                         "contractPhone"=>"18840287763"
+                                                         "principalIdentifyCardFrontPhoto"=>"1xxxxxxxxxx",
+                                                         "principalIdentifyCardBackPhoto"=>"2xxxxxxxxxx",
+                                                         "principalIdentifyCardHandHeldPhoto"=>"3xxxxxxxxxx",
+                                                         "principalBirthday"=>"2010-10-10",
+                                                         "principalBirthdayCategory"=>2,
+                                                         "contactName"=>"nijinghello",
+                                                         "contactDuties"=>"程序员",
+                                                         "contactPhone"=>"18840287763"
                                                          )
                                        )
                           );
 $response = $client->request(
                 'PUT',
-                'http://127.0.0.1/applyCompanyForms/2',
+                'applyCompanyForms/1',
                 [
                 'haders'=>['Content-Type' => 'application/vnd.api+json'],
                 'json' => $data
@@ -68,7 +83,7 @@ $response = $client->request(
 //confirm --start
 /*$response = $client->request(
                 'PUT',
-                'http://127.0.0.1/applyCompanyForms/4/confirm',
+                'applyCompanyForms/4/confirm',
                 [
                 'haders'=>['Content-Type' => 'application/vnd.api+json'],
                 ]
@@ -85,7 +100,7 @@ $response = $client->request(
 
 $response = $client->request(
                 'PUT',
-                'http://127.0.0.1/applyCompanyForms/6/decline',
+                'applyCompanyForms/6/decline',
                 [
                 'haders'=>['Content-Type' => 'application/vnd.api+json'],
                 'json' => $data
@@ -96,7 +111,7 @@ $response = $client->request(
 //get replyApplyCompanyForm --start
 /*$response = $client->request(
                 'GET',
-                'http://127.0.0.1/replyApplyCompanyForms?filter[applyCompanyFormId]=4',
+                'replyApplyCompanyForms?filter[applyCompanyFormId]=4',
                 [
                 'haders'=>['Content-Type' => 'application/vnd.api+json'],
                 ]
@@ -109,4 +124,54 @@ $contents = $body->getContents();
 
 echo "<pre>";
 var_dump($contents);
+
+/*
+{
+    "meta": [],
+    "data": {
+        "type": "applyCompanyForms",
+        "id": "1",
+        "attributes": {
+            "name": "\u4f01\u4e1a\u540d\u79f0555",
+            "registrationNumber": "xxxxxxxxxx",
+            "unifiedSocialCreditCode": "xxxxxxxxxx",
+            "certificate": "certificate1,certificate2,certificate3",
+            "province": 20,
+            "city": 30,
+            "region": 40,
+            "contactPhone": "18840287763",
+            "contactName": "nijinghello",
+            "contactDuties": "\u7a0b\u5e8f\u5458",
+            "principalRealName": "\u6cd5\u4eba\u540d\u5b57",
+            "principalIdentifyCardNumber": "123456798962",
+            "principalIdentifyCardFrontPhoto": "1xxxxxxxxxx",
+            "principalIdentifyCardBackPhoto": "2xxxxxxxxxx",
+            "principalIdentifyCardHandHeldPhoto": "3xxxxxxxxxx",
+            "principalBirthday": "2010-10-10",
+            "principalBirthdayCategory": 2,
+            "status": 0,
+            "updateTime": 1484033686,
+            "createTime": 1484033191,
+            "statusTime": 1484033686
+        },
+        "relationships": {
+            "users": {
+                "data": {
+                    "type": "users",
+                    "id": "1"
+                }
+            },
+            "companies": {
+                "data": {
+                    "type": "companies",
+                    "id": "0"
+                }
+            }
+        },
+        "links": {
+            "self": "127.0.0.1\/applyCompanyForms\/1"
+        }
+    }
+}
+*/
 
--- company.api.qixinyun.com/CompanyUtilTest.php
+++ company.api.qixinyun.com/CompanyUtilTest.php
@@ -5,81 +5,77 @@ require '../vendor/autoload.php';
 use GuzzleHttp;
 
 $client = new GuzzleHttp\Client();
-//get 
-// filter[principalRealName]
-// filter[name]
-// filter[uid]
-// filter[registrationCapital]
-// filter[industry]
-// filter[businessStatus]
-// filter[areaId]
-// filter[establishedPeriod]
 
-/*$response = $client->request(
-                'GET',
-                'http://127.0.0.1/companies?filter[principalRealName]=法人名字',
-                [
-                'haders'=>['Content-Type' => 'application/vnd.api+json'],
-                ]
-            );*/
+//$client = new GuzzleHttp\Client(['base_uri' => 'http://139.224.65.141:8003/']);
+$client = new GuzzleHttp\Client(['base_uri' => 'http://127.0.0.1/']);
+
+//get  filter[name]  filter[principalRealName] filter[industry]  filter[unifiedSocialCreditCode] filter[registrationNumber]  filter[registrationCapital]
+//sort=-id  sort=-name  sort=-principalRealName
+$response = $client->request('GET', 'companies?filter[unifiedSocialCreditCode]=91350100095309691A',['haders'=>['Content-' => 'application/vnd.api+json']]);
+// $response = $client->request('GET', 'companies/1,2',['haders'=>['Content-' => 'application/vnd.api+json']]);
+// $response = $client->request('GET', 'companies?filter[registrationCapital]=20000',['haders'=>['Content-' => 'application/vnd.api+json']]);
 
 /**
  * 
  * certificate | array 第三方图片地址 三证/一证  address1，address2，address3
  *
  */
-$data = array("data"=>array("type"=>"companies",
-                                    "attributes"=>array( "name"=>"企业名称44",
-                                                         "certificate"=>array('address1','222','333'),
-                                                         "province"=>20,
-                                                         "city"=>274,                                                        
-                                                         "region"=>2288,
+/*$data = array("data"=>array("type"=>"companies",
+                                    "attributes"=>array( "name"=>"企业2255",
+                                                         "certificate"=>array('address11','address22','address33'),
+                                                         "province"=>2,
+                                                         "city"=>52,                                                        
+                                                         "region"=>500,
                                                          "uid"=>1,
                                                          "principalRealName"=>"法人名字",
                                                          "principalIdentifyCardNumber"=>"xxxxxxxxxx",
-                                                         "principalCertificate"=>"xxxxxxxxxx",
-                                                         "contractPhone"=>"1234568911",
+                                                         "principalIdentifyCardFrontPhoto"=>"11xxxxxxxxxx",
+                                                         "principalIdentifyCardBackPhoto"=>"22xxxxxxxxxx",
+                                                         "principalIdentifyCardHandHeldPhoto"=>"33xxxxxxxxxx",
+                                                         "contactPhone"=>"1234568911",
                                                          "category"=>1,
                                                          "registrationNumber"=>"xxxxxxxxxx",
                                                          "unifiedSocialCreditCode"=>"xxxxxxxxxx",
                                                          "organizationCode"=>"xxxxxxxxxx",
                                                          "registrationAuthority"=>"登记机关",
-                                                         "approveDate"=>"123456789",
+                                                         "approveDate"=>"2016-12-31",
                                                          "englishName"=>"xxxxxxxxxx",
                                                          "registrationCapital"=>20000,
                                                          "scale"=>1,
-                                                         "establishedDate"=>"123456789",
+                                                         "establishedDate"=>"2016-12-31",
                                                          "industry"=>1,
-                                                         "issuedDate"=>"123456789",
+                                                         "issuedDate"=>"2016-12-31",
                                                          "creditUrl"=>"xxxxxxxxxx",
                                                          "email"=>"xxxxxxxxxx",
                                                          "postalCode"=>"710000",
-                                                         "businessTermFrom"=>"123456789",
-                                                         "businessTermTo"=>"123456789",
+                                                         "businessTermFrom"=>"2016-12-31",
+                                                         "businessTermTo"=>"2016-12-31",
                                                          "businessScope"=>"xxxxxxxxxx",
                                                          "businessStatus"=>2,
                                                          "principalCategory"=>1,
+                                                         "principalBirthday"=>"2010-10-10",
+                                                         "principalBirthdayCategory"=>2,
+                                                         "contactName"=>"nijinghello",
+                                                         "contactDuties"=>"程序员",
                                                          "detailedAddress"=>"详细地址"
                                                          )
                                        )
-                          );
-
+                          );*/
 //add --start
 /*$response = $client->request(
                 'POST',
-                'http://127.0.0.1/companies',
+                'companies',
                 [
                 'haders'=>['Content-Type' => 'application/vnd.api+json'],
                 'json' => $data
                 ]
-            );
-*/
+            );*/
 //add --end
 
 //edit --start
 /*$response = $client->request(
                 'PUT',
-                'http://127.0.0.1/companies/4',
+                'http://127.0.0.1/companies/101',
                 [
                 'haders'=>['Content-Type' => 'application/vnd.api+json'],
                 'json' => $data
@@ -95,3 +91,72 @@ $contents = $body->getContents();
 echo "<pre>";
 var_dump($contents);
 
+
+/*
+{
+    "meta": [],
+    "data": {
+        "type": "companies",
+        "id": "101",
+        "attributes": {
+            "name": "\u4f01\u4e1a55552",
+            "category": 1,
+            "certificate": [
+                "address1",
+                "address2",
+                "address3"
+            ],
+            "registrationNumber": "xxxxxxxxxx",
+            "unifiedSocialCreditCode": "xxxxxxxxxx",
+            "organizationCode": "xxxxxxxxxx",
+            "principalRealName": "\u6cd5\u4eba\u540d\u5b57",
+            "principalIdentifyCardNumber": "xxxxxxxxxx",
+            "principalIdentifyCardFrontPhoto": "1xxxxxxxxxx",
+            "principalIdentifyCardBackPhoto": "2xxxxxxxxxx",
+            "principalIdentifyCardHandHeldPhoto": "3xxxxxxxxxx",
+            "principalCategory": 1,
+            "principalBirthday": "2011-10-10",
+            "principalBirthdayCategory": 1,
+            "province": 2,
+            "city": 52,
+            "region": 500,
+            "businessTermFrom": "2016-12-31",
+            "businessTermTo": "2016-12-31",
+            "businessScope": "xxxxxxxxxx",
+            "businessStatus": 2,
+            "registrationAuthority": "\u767b\u8bb0\u673a\u5173",
+            "approveDate": "2016-12-31",
+            "englishName": "xxxxxxxxxx",
+            "registrationCapital": 20000,
+            "scale": 1,
+            "establishedDate": "2016-12-31",
+            "industry": 1,
+            "issuedDate": "2016-12-31",
+            "creditUrl": "xxxxxxxxxx",
+            "contactPhone": "1234568911",
+            "email": "xxxxxxxxxx",
+            "postalCode": "710000",
+            "contactName": "nijing",
+            "contactDuties": "\u7a0b\u5e8f\u5458",
+            "detailedAddress": "\u8be6\u7ec6\u5730\u5740",
+            "readCount": 0,
+            "searchCount": 0,
+            "status": 0,
+            "statusTime": 0,
+            "updateTime": 0,
+            "createTime": 1484015798
+        },
+        "relationships": {
+            "users": {
+                "data": {
+                    "type": "users",
+                    "id": "1"
+                }
+            }
+        },
+        "links": {
+            "self": "127.0.0.1\/companies\/101"
+        }
+    }
+}
+*/
