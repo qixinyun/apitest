91ea619618c896b7954d35c81a782f76128fc7f4
leonNJ <1119679388@qq.com>
2017-01-18 20:50:36 +0800

    添加合同服务测试

--- contract.api.qixinyun.com/ApplyContractFormUtilTest.php
+++ contract.api.qixinyun.com/ApplyContractFormUtilTest.php
@@ -7,13 +7,32 @@ use GuzzleHttp;
 // $client = new GuzzleHttp\Client(['base_uri' => 'http://139.224.65.136:8007/']);
  $client = new GuzzleHttp\Client(['base_uri' => 'http://127.0.0.1/']);
 
-//get 
-// $response = $client->request('GET', 'applyContractForms',['haders'=>['Content-' => 'application/vnd.api+json']]);
+//get applyContractForms
+  /**
+     *   3.1 filter[status]
+     *   3.2 filter[name]
+     *   3.3 filter[uid]
+     *   3.4 filter[userGroupId]
+     *   3.5 filter[departmentId]
+     */
+// $response = $client->request('GET', 'applyContractForms/2',['haders'=>['Content-' => 'application/vnd.api+json']]);
 // $response = $client->request('GET', 'applyContractForms/1,2',['haders'=>['Content-' => 'application/vnd.api+json']]);
-// $response = $client->request('GET', 'applyContractForms?filter[userGroupId]=10&sort=-id',['haders'=>['Content-' => 'application/vnd.api+json']]);
+// $response = $client->request('GET', 'applyContractForms?filter[name]=qq&sort=-id',['haders'=>['Content-' => 'application/vnd.api+json']]);
+
+//get contract
+   /**
+     *   3.1 filter[status]
+     *   3.2 filter[name]
+     *   3.3 filter[userGroupId]
+     *   3.4 filter[departmentId]
+     *   3.5 filter[price]
+     */
+// $response = $client->request('GET', 'contracts',['haders'=>['Content-' => 'application/vnd.api+json']]);
+ //$response = $client->request('GET', 'contracts/1,2',['haders'=>['Content-' => 'application/vnd.api+json']]);
+ $response = $client->request('GET', 'contracts?filter[departmentId]=9&sort=-id',['haders'=>['Content-' => 'application/vnd.api+json']]);
 
 //add -- 开始
-$data = array("data"=>array("type"=>"applyContractForms",
+/*$data = array("data"=>array("type"=>"applyContractForms",
                             "attributes"=>array("name"=>"contractName",
                                                 "validityStart"=>'2017-01-18',
                                                 "validityEnd"=>'2027-01-18',
@@ -32,7 +51,7 @@ $response = $client->request(
                 'haders'=>['Content-Type' => 'application/vnd.api+json'],
                 'json' => $data
                 ]
-            );
+            );*/
 //add -- 结束
 
 //edit -- 开始
