210f83757e3c6585d27724d9238d4043a973afbf
leonNJ <1119679388@qq.com>
2017-01-19 16:54:20 +0800

    合同服务添加业务

--- contract.api.qixinyun.com/ApplyContractFormUtilTest.php
+++ contract.api.qixinyun.com/ApplyContractFormUtilTest.php
@@ -4,8 +4,8 @@ namespace Product;
 require '../vendor/autoload.php';
 use GuzzleHttp;
 
-$client = new GuzzleHttp\Client(['base_uri' => 'http://139.224.65.141:8006/']);
- // $client = new GuzzleHttp\Client(['base_uri' => 'http://127.0.0.1/']);
+ $client = new GuzzleHttp\Client(['base_uri' => 'http://139.224.65.141:8006/']);
+// $client = new GuzzleHttp\Client(['base_uri' => 'http://127.0.0.1/']);
 
 //get applyContractForms
   /**
@@ -14,10 +14,11 @@ $client = new GuzzleHttp\Client(['base_uri' => 'http://139.224.65.141:8006/']);
      *   3.3 filter[uid]
      *   3.4 filter[userGroupId]
      *   3.5 filter[departmentId]
+     *   3.6 filter[businessId]
      */
-// $response = $client->request('GET', 'applyContractForms/2',['haders'=>['Content-' => 'application/vnd.api+json']]);
+ $response = $client->request('GET', 'applyContractForms/1',['haders'=>['Content-' => 'application/vnd.api+json']]);
 // $response = $client->request('GET', 'applyContractForms/1,2',['haders'=>['Content-' => 'application/vnd.api+json']]);
-// $response = $client->request('GET', 'applyContractForms?filter[name]=qq&sort=-id',['haders'=>['Content-' => 'application/vnd.api+json']]);
+// $response = $client->request('GET', 'applyContractForms?filter[businessId]=1&sort=-id',['haders'=>['Content-' => 'application/vnd.api+json']]);
 
 //get contract
    /**
@@ -26,13 +27,14 @@ $client = new GuzzleHttp\Client(['base_uri' => 'http://139.224.65.141:8006/']);
      *   3.3 filter[userGroupId]
      *   3.4 filter[departmentId]
      *   3.5 filter[price]
+     *   3.6 filter[businessId]
      */
 // $response = $client->request('GET', 'contracts',['haders'=>['Content-' => 'application/vnd.api+json']]);
  //$response = $client->request('GET', 'contracts/1,2',['haders'=>['Content-' => 'application/vnd.api+json']]);
- // $response = $client->request('GET', 'contracts?filter[departmentId]=9&sort=-id',['haders'=>['Content-' => 'application/vnd.api+json']]);
+// $response = $client->request('GET', 'contracts?filter[businessId]=2&sort=-id',['haders'=>['Content-' => 'application/vnd.api+json']]);
 
 //add -- 开始
-$data = array("data"=>array("type"=>"applyContractForms",
+/*$data = array("data"=>array("type"=>"applyContractForms",
                             "attributes"=>array("name"=>"contractName",
                                                 "validityStart"=>'2017-01-18',
                                                 "validityEnd"=>'2027-01-18',
@@ -40,6 +42,7 @@ $data = array("data"=>array("type"=>"applyContractForms",
                                                 "price"=>10.2,
                                                 "companyId"=>1,
                                                 "productId"=>1,
+                                                "businessId"=>1,
                                                 "uid"=>86
                                                )
                            )
@@ -51,29 +54,38 @@ $response = $client->request(
                 'haders'=>['Content-Type' => 'application/vnd.api+json'],
                 'json' => $data
                 ]
-            );
+            );*/
 //add -- 结束
 
 //edit -- 开始
-// $data = array("data"=>array("type"=>"applyContractForms",
-//                             "attributes"=>array("name"=>"contractNameqqq",
-//                                                 "validityStart"=>'2017-01-28',
-//                                                 "validityEnd"=>'2027-01-28',
-//                                                 "category"=>2,
-//                                                 "price"=>10.25,
-//                                                 "productId"=>1
-//                                                )
-//                            )
-//               );
-// $response = $client->request(
-//                 'PUT',
-//                 'applyContractForms/4',
-//                 [
-//                 'haders'=>['Content-Type' => 'application/vnd.api+json'],
-//                 'json' => $data
-//                 ]
-//             );
+/*$data = array("data"=>array("type"=>"applyContractForms",
+                            "attributes"=>array("name"=>"contractNameqqq",
+                                                "validityStart"=>'2017-01-28',
+                                                "validityEnd"=>'2027-01-28',
+                                                "category"=>2,
+                                                "price"=>10.25,
+                                                "productId"=>1
+                                               )
+                           )
+              );
+$response = $client->request(
+                'PUT',
+                'applyContractForms/1',
+                [
+                'haders'=>['Content-Type' => 'application/vnd.api+json'],
+                'json' => $data
+                ]
+            );*/
 
+/*$response = $client->request(
+                'PUT',
+                'applyContractForms/1/decline',
+                [
+                'haders'=>['Content-Type' => 'application/vnd.api+json'],
+                'json' => $data
+                ]
+            );
+*/
 $status = $response->getStatusCode();
 $body = $response->getBody();
 $contents = $body->getContents();
@@ -82,3 +94,137 @@ $contents = $body->getContents();
 echo "<pre>";
 print_r($status);
 print_r($contents);
+
+/*
+{
+    "meta": [],
+    "data": {
+        "type": "applyContractForms",
+        "id": "1",
+        "attributes": {
+            "name": "contractNameqqq",
+            "validityStart": "2017-01-28",
+            "validityEnd": "2027-01-28",
+            "category": 2,
+            "price": 10,
+            "status": -2,
+            "updateTime": 1484662497,
+            "createTime": 1484660426,
+            "statusTime": 1484741139
+        },
+        "relationships": {
+            "user": {
+                "data": {
+                    "type": "users",
+                    "id": "86"
+                }
+            },
+            "userGroup": {
+                "data": {
+                    "type": "userGroups",
+                    "id": "2"
+                }
+            },
+            "department": {
+                "data": {
+                    "type": "departments",
+                    "id": "9"
+                }
+            },
+            "company": {
+                "data": {
+                    "type": "companies",
+                    "id": "1"
+                }
+            },
+            "product": {
+                "data": {
+                    "type": "products",
+                    "id": "1"
+                }
+            },
+            "business": {
+                "data": {
+                    "type": "businesses",
+                    "id": "1"
+                }
+            }
+        },
+        "links": {
+            "self": "127.0.0.1\/applyContractForms\/1"
+        }
+    }
+}
+*/
+
+/*
+{
+    "meta": {
+        "count": 1
+    },
+    "links": {
+        "first": null,
+        "last": null,
+        "prev": null,
+        "next": null
+    },
+    "data": [
+        {
+            "type": "contracts",
+            "id": "1",
+            "attributes": {
+                "name": "contractName",
+                "validityStart": "2017-01-18",
+                "validityEnd": "2027-01-18",
+                "category": 1,
+                "price": 10,
+                "updateTime": 0,
+                "createTime": 1484740956,
+                "statusTime": 0,
+                "status": 0
+            },
+            "relationships": {
+                "user": {
+                    "data": {
+                        "type": "users",
+                        "id": "86"
+                    }
+                },
+                "userGroup": {
+                    "data": {
+                        "type": "userGroups",
+                        "id": "2"
+                    }
+                },
+                "department": {
+                    "data": {
+                        "type": "departments",
+                        "id": "9"
+                    }
+                },
+                "company": {
+                    "data": {
+                        "type": "companies",
+                        "id": "1"
+                    }
+                },
+                "product": {
+                    "data": {
+                        "type": "products",
+                        "id": "1"
+                    }
+                },
+                "business": {
+                    "data": {
+                        "type": "businesses",
+                        "id": "2"
+                    }
+                }
+            },
+            "links": {
+                "self": "127.0.0.1\/contracts\/1"
+            }
+        }
+    ]
+}
+*/
