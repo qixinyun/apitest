e184fc674102558ecfa4ce68cc99b4a985177458
leonNJ <1119679388@qq.com>
2017-04-05 11:07:41 +0800

    认领企业接口测试

--- company.api.qixinyun.com/ApplyCompanyUtilTest.php
+++ company.api.qixinyun.com/ApplyCompanyUtilTest.php
@@ -7,8 +7,7 @@ use GuzzleHttp;
 //$client = new GuzzleHttp\Client(['base_uri' => 'http://139.224.65.141:8003/']);
 $client = new GuzzleHttp\Client(['base_uri' => 'http://127.0.0.1/']);
 
-//get  filter[name]  filter[principalRealName] 
-//sort=-id  sort=-name  sort=-principalRealName
+//get  filter[name]  filter[principalRealName] filter[uid] filter[status]
 // $response = $client->request('GET', 'applyCompanyForms?sort=-principalRealName',['haders'=>['Content-' => 'application/vnd.api+json']]);
 // $response = $client->request('GET', 'applyCompanyForms/1,2',['haders'=>['Content-' => 'application/vnd.api+json']]);
 // $response = $client->request('GET', 'applyCompanyForms?filter[name]=555&filter[principalRealName]=ni',['haders'=>['Content-' => 'application/vnd.api+json']]);
