d55d244c8f01139c9f7e2ba408ff4419456a8a43
leonNJ <1119679388@qq.com>
2017-04-12 12:38:53 +0800

    股权变更信息接口测试文件

--- company.api.qixinyun.com/EquityChangeTest.php
+++ company.api.qixinyun.com/EquityChangeTest.php
@@ -10,15 +10,21 @@ use GuzzleHttp;
 //get 
  //filter[status]
  //filter[company]
- $response = $client->request('GET', 'equityChanges',['haders'=>['Content-' => 'application/vnd.api+json']]);
+// $response = $client->request('GET', 'equityChanges',['haders'=>['Content-' => 'application/vnd.api+json']]);
 // $response = $client->request('GET', 'equityChanges/1,2',['haders'=>['Content-' => 'application/vnd.api+json']]);
 // $response = $client->request('GET', 'equityChanges?sort=-id',['haders'=>['Content-' => 'application/vnd.api+json']]);
 
 //add -- 开始
 /*$data = array("data"=>array("type"=>"equityChanges",
-                            "attributes"=>array("name"=>"namenamename",
-                                                "members"=>"membersmembers",
-                                                "maxClearCount"=>12,
+                            "attributes"=>array("beforeChangeEquityRatio"=>"beforeChangeEquityRatio",
+                                                "afterhangeEquityRatio"=>"afterhangeEquityRatio",
+                                                "date"=>"2010-01-01",
+                                                "executor"=>"executor",
+                                                "equityAmount"=>"equityAmount",
+                                                "assignee"=>"assignee",
+                                                "court"=>"court",
+                                                "content"=>"contentcontent",
+                                                "shareholderId"=>1,
                                                 "companyId"=>1
                                                )
                            )
@@ -34,21 +40,26 @@ $response = $client->request(
 //add -- 结束
 
 //edit -- 开始
-/*$data = array("data"=>array("type"=>"equityChanges",
-                            "attributes"=>array("name"=>"namenamename",
-                                                "members"=>"membersmembers",
-                                                "maxClearCount"=>12,
+$data = array("data"=>array("type"=>"equityChanges",
+                            "attributes"=>array("beforeChangeEquityRatio"=>"1beforeChangeEquityRatio",
+                                                "afterhangeEquityRatio"=>"1afterhangeEquityRatio",
+                                                "date"=>"2011-01-01",
+                                                "executor"=>"1executor",
+                                                "equityAmount"=>"1equityAmount",
+                                                "assignee"=>"1assignee",
+                                                "court"=>"1court",
+                                                "content"=>"1contentcontent"
                                                )
                            )
               );
 $response = $client->request(
                 'PUT',
-                'equityChanges/1',
+                'equityChanges/2',
                 [
                 'haders'=>['Content-Type' => 'application/vnd.api+json'],
                 'json' => $data
                 ]
-            );*/
+            );
 
 $status = $response->getStatusCode();
 $body = $response->getBody();
@@ -59,51 +70,41 @@ echo "<pre>";
 print_r($status);
 print_r($contents);
 
-/*{{
-    "meta": {
-        "count": 1
-    },
-    "links": {
-        "first": null,
-        "last": null,
-        "prev": null,
-        "next": null
-    },
-    "data": [
-        {
-            "type": "equityChanges",
-            "id": "1",
-            "attributes": {
-                "beforeChangeequityRatio": "\u53d8\u66f4\u524d\u80a1\u6743\u6bd4\u4f8b",
-                "afterhangeequityRatio": "\u53d8\u66f4\u540e\u80a1\u6743\u6bd4\u4f8b",
-                "date": "2017-03-08",
-                "executor": "\u88ab\u6267\u884c\u4eba",
-                "equityAmount": "\u80a1\u6743\u6570\u989d",
-                "assignee": "\u53d7\u8ba9\u4eba",
-                "court": "\u6267\u884c\u6cd5\u9662",
-                "content": "\u8be6\u60c5",
-                "status": 0,
-                "statusTime": 0,
-                "updateTime": 0,
-                "createTime": 1487752865
-            },
-            "relationships": {
-                "companies": {
-                    "data": {
-                        "type": "companies",
-                        "id": "1"
-                    }
-                },
-                "shareholders": {
-                    "data": {
-                        "type": "shareholders",
-                        "id": "2"
-                    }
+/*{
+    "meta": [],
+    "data": {
+        "type": "equityChanges",
+        "id": "2",
+        "attributes": {
+            "beforeChangeequityRatio": "beforeChangeEquityRatio",
+            "afterhangeequityRatio": "afterhangeEquityRatio",
+            "date": "2010-01-01",
+            "executor": "executor",
+            "equityAmount": "equityAmount",
+            "assignee": "assignee",
+            "court": "court",
+            "content": "contentcontent",
+            "status": 0,
+            "statusTime": 0,
+            "updateTime": 0,
+            "createTime": 1491965072
+        },
+        "relationships": {
+            "companies": {
+                "data": {
+                    "type": "companies",
+                    "id": "1"
                 }
             },
-            "links": {
-                "self": "127.0.0.1\/equityChanges\/1"
+            "shareholders": {
+                "data": {
+                    "type": "shareholders",
+                    "id": "1"
+                }
             }
+        },
+        "links": {
+            "self": "127.0.0.1\/equityChanges\/2"
         }
-    ]
+    }
 }*/
