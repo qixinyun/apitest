8b95aaee8a0d283f7d07fb6472667eaf4653863d
leonNJ <1119679388@qq.com>
2017-05-13 15:12:36 +0800

    新闻动态信息申请表单接口测试文件

--- company.api.qixinyun.com/ApplyNewsFormTest.php
+++ company.api.qixinyun.com/ApplyNewsFormTest.php
@@ -13,7 +13,7 @@ use GuzzleHttp;
  //filter[company]
  //filter[uid]
  //filter[userIdentification]
-// $response = $client->request('GET', 'applyNewsForms',['haders'=>['Content-' => 'application/vnd.api+json']]);
+ $response = $client->request('GET', 'applyNewsForms',['haders'=>['Content-' => 'application/vnd.api+json']]);
 // $response = $client->request('GET', 'applyNewsForms/1,2',['haders'=>['Content-' => 'application/vnd.api+json']]);
 // $response = $client->request('GET', 'applyNewsForms?filter[company]=1&sort=-id',['haders'=>['Content-' => 'application/vnd.api+json']]);
 
