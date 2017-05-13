92a2d48e078fbb70e6f455903464542684e62a5f
leonNJ <1119679388@qq.com>
2017-05-13 15:18:39 +0800

    撤销信息申请表单接口测试文件

--- company.api.qixinyun.com/ApplyRevocationInfoFormTest.php
+++ company.api.qixinyun.com/ApplyRevocationInfoFormTest.php
@@ -11,7 +11,7 @@ use GuzzleHttp;
  //filter[status]
  //filter[uid]
  //filter[userIdentification]
- $response = $client->request('GET', 'applyRevocationInfoForms/31,32',['haders'=>['Content-' => 'application/vnd.api+json']]);
+// $response = $client->request('GET', 'applyRevocationInfoForms/31,32',['haders'=>['Content-' => 'application/vnd.api+json']]);
 // $response = $client->request('GET', 'applyRevocationInfoForms/1,2',['haders'=>['Content-' => 'application/vnd.api+json']]);
 // $response = $client->request('GET', 'applyRevocationInfoForms?filter[userIdentification]=2&sort=-id',['haders'=>['Content-' => 'application/vnd.api+json']]);
 
