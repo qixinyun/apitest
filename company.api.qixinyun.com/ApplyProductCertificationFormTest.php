29daa0d50a1809068e7a478bb40b9c050a284cab
leonNJ <1119679388@qq.com>
2017-05-13 15:15:47 +0800

    产品体系认证信息申请表单接口测试文件

--- company.api.qixinyun.com/ApplyProductCertificationFormTest.php
+++ company.api.qixinyun.com/ApplyProductCertificationFormTest.php
@@ -15,7 +15,7 @@ use GuzzleHttp;
  //filter[company]
  //filter[uid]
  //filter[userIdentification]
- $response = $client->request('GET', 'applyProductCertificationForms/1',['haders'=>['Content-' => 'application/vnd.api+json']]);
+// $response = $client->request('GET', 'applyProductCertificationForms/1',['haders'=>['Content-' => 'application/vnd.api+json']]);
 // $response = $client->request('GET', 'applyProductCertificationForms/1,2',['haders'=>['Content-' => 'application/vnd.api+json']]);
 // $response = $client->request('GET', 'applyProductCertificationForms?filter[company]=1&sort=-id',['haders'=>['Content-' => 'application/vnd.api+json']]);
 
