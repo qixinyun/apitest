150423855b718a427aadd36c848363cf9c0c834c
leonNJ <1119679388@qq.com>
2017-05-13 15:19:45 +0800

    交易评价信息申请表单接口测试文件

--- company.api.qixinyun.com/ApplyTransactionEvaluateFormTest.php
+++ company.api.qixinyun.com/ApplyTransactionEvaluateFormTest.php
@@ -12,7 +12,7 @@ use GuzzleHttp;
  //filter[company]
  //filter[uid]
  //filter[userIdentification]
-// $response = $client->request('GET', 'applyTransactionEvaluateForms',['haders'=>['Content-' => 'application/vnd.api+json']]);
+ $response = $client->request('GET', 'applyTransactionEvaluateForms',['haders'=>['Content-' => 'application/vnd.api+json']]);
 // $response = $client->request('GET', 'applyTransactionEvaluateForms/1,2',['haders'=>['Content-' => 'application/vnd.api+json']]);
 // $response = $client->request('GET', 'applyTransactionEvaluateForms?filter[company]=1&sort=-id',['haders'=>['Content-' => 'application/vnd.api+json']]);
 
