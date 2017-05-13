d767ce6209853648d6508ebbc2160522dd9ad4cd
leonNJ <1119679388@qq.com>
2017-05-13 15:16:06 +0800

    产品信息申请表单接口测试文件

--- company.api.qixinyun.com/ApplyProductInfoFormTest.php
+++ company.api.qixinyun.com/ApplyProductInfoFormTest.php
@@ -13,7 +13,7 @@ use GuzzleHttp;
  //filter[company]
  //filter[uid]
  //filter[userIdentification]
-// $response = $client->request('GET', 'applyProductInfoForms',['haders'=>['Content-' => 'application/vnd.api+json']]);
+ $response = $client->request('GET', 'applyProductInfoForms',['haders'=>['Content-' => 'application/vnd.api+json']]);
 // $response = $client->request('GET', 'applyProductInfoForms/1,2',['haders'=>['Content-' => 'application/vnd.api+json']]);
 // $response = $client->request('GET', 'applyProductInfoForms?filter[company]=1&sort=-id',['haders'=>['Content-' => 'application/vnd.api+json']]);
 
