0d540a48dfbe1b31b2c065078137a9a7b61656a6
leonNJ <1119679388@qq.com>
2017-05-13 15:03:43 +0800

    分支机构信息申请表单接口测试文件

--- company.api.qixinyun.com/ApplyCompanyBranchFormTest.php
+++ company.api.qixinyun.com/ApplyCompanyBranchFormTest.php
@@ -11,7 +11,7 @@ use GuzzleHttp;
  //filter[status]
  //filter[uid]
  //filter[userIdentification]
-// $response = $client->request('GET', 'applyCompanyBranchForms/1',['haders'=>['Content-' => 'application/vnd.api+json']]);
+ $response = $client->request('GET', 'applyCompanyBranchForms/1',['haders'=>['Content-' => 'application/vnd.api+json']]);
 // $response = $client->request('GET', 'applyCompanyBranchForms/1,2',['haders'=>['Content-' => 'application/vnd.api+json']]);
 // $response = $client->request('GET', 'applyCompanyBranchForms?filter[category]=1&sort=-id',['haders'=>['Content-' => 'application/vnd.api+json']]);
 
