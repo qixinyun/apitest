0e59c647c9392a10ce0952d22df140f45ef807b0
leonNJ <1119679388@qq.com>
2017-05-13 15:31:15 +0800

    关联企业信息申请表单接口测试文件

--- company.api.qixinyun.com/AssociatedCompanyTest.php
+++ company.api.qixinyun.com/AssociatedCompanyTest.php
@@ -14,7 +14,7 @@ use GuzzleHttp;
  //filter[company]
 // $response = $client->request('GET', 'associatedCompanies',['haders'=>['Content-' => 'application/vnd.api+json']]);
 // $response = $client->request('GET', 'associatedCompanies/1,2',['haders'=>['Content-' => 'application/vnd.api+json']]);
- $response = $client->request('GET', 'associatedCompanies?filter[category]=1&sort=-id',['haders'=>['Content-' => 'application/vnd.api+json']]);
+// $response = $client->request('GET', 'associatedCompanies?filter[category]=1&sort=-id',['haders'=>['Content-' => 'application/vnd.api+json']]);
 
 //add -- 开始
 /*$data = array("data"=>array("type"=>"associatedCompanies",
