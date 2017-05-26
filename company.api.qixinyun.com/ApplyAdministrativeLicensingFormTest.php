22c8010f7595414626166bfa7b0a6030337262da
leonNJ <1119679388@qq.com>
2017-05-26 18:24:40 +0800

    行政许可信息申请表单接口测试文件

--- company.api.qixinyun.com/ApplyAdministrativeLicensingFormTest.php
+++ company.api.qixinyun.com/ApplyAdministrativeLicensingFormTest.php
@@ -16,20 +16,21 @@ use GuzzleHttp;
 // $response = $client->request('GET', 'applyAdministrativeLicensingForms?filter[userIdentification]=2&sort=-id',['haders'=>['Content-' => 'application/vnd.api+json']]);
 
 //add -- 开始
-$data = array("data"=>array("type"=>"applyAdministrativeLicensingForms",
+/*$data = array("data"=>array("type"=>"applyAdministrativeLicensingForms",
                             "attributes"=>array("number"=>'331112222233333',
                                                 "title"=>'33titletitletitle1',
                                                 "validityFrom"=>'2019-05-05',
                                                 "validityTo"=>'2039-05-05',
-                                                "content"=>'3311contentcontentcontent',
-                                                "council"=>'3311councilcouncilcouncilcouncil',
-                                                "detail"=>'1331detaildetaildetaildetail',
+                                                "content"=>'222ontentcontentcontent',
+                                                "council"=>'3222councilcouncilcouncilcouncil',
+                                                "detail"=>'1322221detaildetaildetaildetail',
                                                 "category"=>2,
                                                 "attachments"=>array('atta33chments125','attach33ments235','a33ttachments345'),
                                                 "administrativeLicensingStatus"=>-2,
                                                 "companyId"=>1,
-                                                "uid"=>1,
-                                                "userIdentification"=>1
+                                                "uid"=>152,
+                                                "userIdentification"=>1,
+                                                "administrativeLicensingId"=>1
                                                ) 
                            )
               );
@@ -40,12 +41,12 @@ $response = $client->request(
                 'haders'=>['Content-Type' => 'application/vnd.api+json'],
                 'json' => $data
                 ]
-            );
+            );*/
 //add -- 结束
 
 //edit -- 开始
-/*$data = array("data"=>array("type"=>"applyAdministrativeLicensingForms",
-                            "attributes"=>array("number"=>'1331112222233333',
+$data = array("data"=>array("type"=>"applyAdministrativeLicensingForms",
+                            "attributes"=>array("number"=>'1232434322412',
                                                 "title"=>'313titletitletitle1',
                                                 "validityFrom"=>'2029-05-05',
                                                 "validityTo"=>'2049-05-05',
@@ -60,12 +61,12 @@ $response = $client->request(
               );
 $response = $client->request(
                 'PUT',
-                'applyAdministrativeLicensingForms/4',
+                'applyAdministrativeLicensingForms/142',
                 [
                 'haders'=>['Content-Type' => 'application/vnd.api+json'],
                 'json' => $data
                 ]
-            );*/
+            );
 //edit -- 结束
 
 
@@ -82,39 +83,40 @@ print_r($contents);
     "meta": [],
     "data": {
         "type": "applyAdministrativeLicensingForms",
-        "id": "8",
+        "id": "142",
         "attributes": {
             "userIdentification": 1,
-            "number": "331112222233333",
-            "title": "33titletitletitle1",
-            "validityFrom": "2019-05-05",
-            "validityTo": "2039-05-05",
-            "content": "3311contentcontentcontent",
-            "council": "3311councilcouncilcouncilcouncil",
-            "detail": "1331detaildetaildetaildetail",
+            "number": "1232434322412",
+            "title": "313titletitletitle1",
+            "validityFrom": "2029-05-05",
+            "validityTo": "2049-05-05",
+            "content": "13311contentcontentcontent",
+            "council": "31311councilcouncilcouncilcouncil",
+            "detail": "11331detaildetaildetaildetail",
             "category": 2,
-            "administrativeLicensingStatus": -2,
+            "administrativeLicensingStatus": 2,
             "attachments": [
-                "atta33chments125",
-                "attach33ments235",
-                "a33ttachments345"
+                "a1tta33chments125",
+                "a1ttach33ments235",
+                "a33tt1achments345"
             ],
             "company": 1,
+            "administrativeLicensingId": 1,
             "status": 0,
-            "statusTime": 0,
-            "updateTime": 0,
-            "createTime": 1492760024
+            "statusTime": 1495793439,
+            "updateTime": 1495793439,
+            "createTime": 1495793362
         },
         "relationships": {
             "user": {
                 "data": {
-                    "type": "users",
-                    "id": "1"
+                    "type": "OAusers",
+                    "id": "152"
                 }
             }
         },
         "links": {
-            "self": "127.0.0.1\/applyAdministrativeLicensingForms\/8"
+            "self": "127.0.0.1\/applyAdministrativeLicensingForms\/142"
         }
     }
 }*/
