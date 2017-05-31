3c88dbe52a47fa69aacb1ea9069bea05e3381308
leonNJ <1119679388@qq.com>
2017-05-31 14:47:58 +0800

    著作权信息申请表单接口测试文件

--- company.api.qixinyun.com/ApplyCopyrightInfoFormsTest.php
+++ company.api.qixinyun.com/ApplyCopyrightInfoFormsTest.php
@@ -14,19 +14,20 @@ use GuzzleHttp;
  //filter[company]
  //filter[uid]
  //filter[userIdentification]
- $response = $client->request('GET', 'applyCopyrightInfoForms/1',['haders'=>['Content-' => 'application/vnd.api+json']]);
+// $response = $client->request('GET', 'applyCopyrightInfoForms/1',['haders'=>['Content-' => 'application/vnd.api+json']]);
 // $response = $client->request('GET', 'applyCopyrightInfoForms/1,2',['haders'=>['Content-' => 'application/vnd.api+json']]);
 // $response = $client->request('GET', 'applyCopyrightInfoForms?filter[registrationCategory]=3&sort=-id',['haders'=>['Content-' => 'application/vnd.api+json']]);
 
 //add -- 开始
 /*$data = array("data"=>array("type"=>"applyCopyrightInfoForms",
-                            "attributes"=>array("name"=>'hellohello',
-                                                "registrationNumber"=>'967845123',
-                                                "registrationDate"=>'2059-05-05',
-                                                "registrationCategory"=>1,
+                            "attributes"=>array("name"=>'111hellohello',
+                                                "registrationNumber"=>'11111967845123',
+                                                "registrationDate"=>'2019-05-05',
+                                                "registrationCategory"=>12,
                                                 "companyId"=>1,
-                                                "uid"=>1,
-                                                "userIdentification"=>1
+                                                "uid"=>152,
+                                                "userIdentification"=>1,
+                                               // "copyrightInfoId"=>1
                                                ) 
                            )
               );
@@ -42,28 +43,28 @@ $response = $client->request(
 
 //edit -- 开始
 $data = array("data"=>array("type"=>"applyCopyrightInfoForms",
-                            "attributes"=>array("name"=>'121worldworld',
-                                                "registrationNumber"=>'1211234567896321',
-                                                "registrationDate"=>'2029-01-15',
+                            "attributes"=>array("name"=>'2222worldworld',
+                                                "registrationNumber"=>'22222222211234567896321',
+                                                "registrationDate"=>'2009-01-15',
                                                 "registrationCategory"=>2,
                                                )
                            )
               );
 $response = $client->request(
                 'PUT',
-                'applyCopyrightInfoForms/1',
+                'applyCopyrightInfoForms/170',
                 [
                 'haders'=>['Content-Type' => 'application/vnd.api+json'],
                 'json' => $data
                 ]
             );
-
+//edit -- 结束
 
 
 $status = $response->getStatusCode();
 $body = $response->getBody();
 $contents = $body->getContents();
-//edit -- 结束
+
 
 echo "<pre>";
 print_r($status);
@@ -73,29 +74,30 @@ print_r($contents);
     "meta": [],
     "data": {
         "type": "applyCopyrightInfoForms",
-        "id": "1",
+        "id": "170",
         "attributes": {
             "userIdentification": 1,
-            "name": "worldworld",
-            "registrationNumber": "1234567896321",
-            "registrationDate": "2009-05-05",
-            "registrationCategory": 2,
+            "name": "111hellohello",
+            "registrationNumber": "11111967845123",
+            "registrationDate": "2019-05-05",
+            "registrationCategory": 12,
             "company": 1,
+            "copyrightInfoId": 1,
             "status": 0,
             "statusTime": 0,
             "updateTime": 0,
-            "createTime": 1488251516
+            "createTime": 1496212904
         },
         "relationships": {
             "user": {
                 "data": {
-                    "type": "users",
-                    "id": "1"
+                    "type": "OAusers",
+                    "id": "152"
                 }
             }
         },
         "links": {
-            "self": "127.0.0.1\/applyCopyrightInfoForms\/1"
+            "self": "127.0.0.1\/applyCopyrightInfoForms\/170"
         }
     }
 }*/
