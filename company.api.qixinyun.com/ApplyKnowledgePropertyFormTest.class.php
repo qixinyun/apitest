9703d39a08f3a58e4b462475271dee4540e1d652
leonNJ <1119679388@qq.com>
2017-06-05 15:14:17 +0800

    知识产权出质信息申请表单测试文件

--- company.api.qixinyun.com/ApplyKnowledgePropertyFormTest.class.php
+++ company.api.qixinyun.com/ApplyKnowledgePropertyFormTest.class.php
@@ -11,13 +11,13 @@ use GuzzleHttp;
  //filter[status]
  //filter[userIdentification]
  //filter[uid]
- $response = $client->request('GET', 'applyKnowledgePropertyForms/1',['haders'=>['Content-' => 'application/vnd.api+json']]);
+// $response = $client->request('GET', 'applyKnowledgePropertyForms/174?include=companies&fields[companies]=establishedDate',['haders'=>['Content-' => 'application/vnd.api+json']]);
 // $response = $client->request('GET', 'applyKnowledgePropertyForms/1,2',['haders'=>['Content-' => 'application/vnd.api+json']]);
 // $response = $client->request('GET', 'applyKnowledgePropertyForms?filter[reason]=1&sort=date',['haders'=>['Content-' => 'application/vnd.api+json']]);
 
 //add -- 开始
 /*$data = array("data"=>array("type"=>"applyKnowledgePropertyForms",
-                            "attributes"=>array("number"=>"221245789631224444",
+                            "attributes"=>array("number"=>"edit1111numbernumber",
                                                 "name"=>'22namename4444',
                                                 "category"=>2,
                                                 "pledgorName"=>'22出质人名称4444',
@@ -25,9 +25,10 @@ use GuzzleHttp;
                                                 "pledgeRegistrationDeadline"=>'2022-09-06',
                                                 "variation"=>'22变化情况2029-05-054444',
                                                 "knowledgePropertyStatus"=>-2,
-                                                "companyId"=>1,
-                                                "uid"=>1,
-                                                "userIdentification"=>1
+                                                "companyId"=>4,
+                                                "uid"=>152,
+                                                "userIdentification"=>1,
+                                                //"knowledgePropertyId"=>1,
                                                )
                            )
               );
@@ -41,27 +42,27 @@ $response = $client->request(
             );*/
 //add -- 结束
 
-//edit -- 开始212121
-/*$data = array("data"=>array("type"=>"applyKnowledgePropertyForms",
-                            "attributes"=>array("number"=>"212121124578963122",
-                                                "name"=>'212121namename',
-                                                "category"=>1,
-                                                "pledgorName"=>'212121出质人名称',
-                                                "pledgeeName"=>'212121质权人名称',
-                                                "pledgeRegistrationDeadline"=>'2021-09-06',
-                                                "variation"=>'212121变化情况2029-05-05',
+//edit -- 开始
+$data = array("data"=>array("type"=>"applyKnowledgePropertyForms",
+                            "attributes"=>array("number"=>"22222editnumbernumber",
+                                                "name"=>'22222212121namename',
+                                                "category"=>12,
+                                                "pledgorName"=>'2222212121出质人名称',
+                                                "pledgeeName"=>'22222212121质权人名称',
+                                                "pledgeRegistrationDeadline"=>'2001-09-06',
+                                                "variation"=>'21212122222222222222222变化情况2029-05-05',
                                                 "knowledgePropertyStatus"=>-2
                                                )
                            )
               );
 $response = $client->request(
                 'PUT',
-                'applyKnowledgePropertyForms/25',
+                'applyKnowledgePropertyForms/176',
                 [
                 'haders'=>['Content-Type' => 'application/vnd.api+json'],
                 'json' => $data
                 ]
-            );*/
+            );
 //edit -- 结束
 
 
@@ -79,33 +80,40 @@ print_r($contents);
     "meta": [],
     "data": {
         "type": "applyKnowledgePropertyForms",
-        "id": "1",
+        "id": "176",
         "attributes": {
             "userIdentification": 1,
-            "number": "1245789631224444",
-            "name": "namename4444",
+            "number": "edit1111numbernumber",
+            "name": "22namename4444",
             "category": 2,
-            "pledgorName": "\u51fa\u8d28\u4eba\u540d\u79f04444",
-            "pledgeeName": "\u8d28\u6743\u4eba\u540d\u79f04444",
-            "pledgeRegistrationDeadline": "2020-09-06",
-            "variation": "\u53d8\u5316\u60c5\u51b52029-05-054444",
-            "knowledgePropertyStatus": 0,
-            "company": 1,
+            "pledgorName": "22\u51fa\u8d28\u4eba\u540d\u79f04444",
+            "pledgeeName": "22\u8d28\u6743\u4eba\u540d\u79f04444",
+            "pledgeRegistrationDeadline": "2022-09-06",
+            "variation": "22\u53d8\u5316\u60c5\u51b52029-05-054444",
+            "knowledgePropertyStatus": -2,
+            "company": 4,
+            "knowledgePropertyId": 1,
             "status": 0,
             "statusTime": 0,
             "updateTime": 0,
-            "createTime": 1487732924
+            "createTime": 1496396111
         },
         "relationships": {
             "user": {
                 "data": {
-                    "type": "users",
-                    "id": "1"
+                    "type": "OAusers",
+                    "id": "152"
+                }
+            },
+            "companies": {
+                "data": {
+                    "type": "companies",
+                    "id": "4"
                 }
             }
         },
         "links": {
-            "self": "127.0.0.1\/applyKnowledgePropertyForms\/1"
+            "self": "127.0.0.1\/applyKnowledgePropertyForms\/176"
         }
     }
 }*/
