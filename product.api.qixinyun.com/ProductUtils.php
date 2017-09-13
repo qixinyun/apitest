22394d1a5ec5498350b1f6b027bf4aac60ec632a
leonnj1990 <111679388@qq.com>
2017-09-13 17:07:21 +0800

    product

--- product.api.qixinyun.com/ProductUtils.php
+++ product.api.qixinyun.com/ProductUtils.php
@@ -8,7 +8,7 @@ use GuzzleHttp;
 $client = new GuzzleHttp\Client(['base_uri' => 'http://127.0.0.1/']);
 
 //get 
-$response = $client->request('GET', 'products/1',
+/*$response = $client->request('GET', 'products/1',
     [
     'headers'        => [
                         'Accept-Encoding' => 'gzip',
@@ -17,61 +17,122 @@ $response = $client->request('GET', 'products/1',
                         ],
     'decode_content' => ''
     ]
-);
+);*/
 // $response = $client->request('GET', 'products/1,2',['haders'=>['Content-' => 'application/vnd.api+json']]);
-// $response = $client->request('GET', 'products?filter[userGroupId]=10&sort=-id',['haders'=>['Content-' => 'application/vnd.api+json']]);
+// $response = $client->request('GET', 'products?filter[number]=123454&sort=-id',['haders'=>['Content-' => 'application/vnd.api+json']]);
 
 //add -- 开始
-// $data = array("data"=>array("type"=>"products",
-//                             "attributes"=>array("name"=>"productName",
-//                                                 "price"=>10.2,
-//                                                 "category"=>0,
-//                                                 "userGroupId"=>10,
-//                                                 "departmentId"=>2,
-//                                                 "content"=>array(
-//                                                             '产品内容'
-//                                                         ),
-//                                                )
-//                            )
-//               );
-// $response = $client->request(
-//                 'POST',
-//                 'products',
-//                 [
-//                 'haders'=>['Content-Type' => 'application/vnd.api+json'],
-//                 'json' => $data
-//                 ]
-//             );
+/*$data = array("data"=>array("type"=>"products",
+                            "attributes"=>array("name"=>"productName",
+                                                "price"=>10.2,
+                                                "category"=>0,
+                                                "userGroupId"=>10,
+                                                "departmentId"=>2,
+                                                "content"=>array(
+                                                            '产品内容'
+                                                        ),
+                                                "number"=>'123454446',
+                                               )
+                           )
+              );
+$response = $client->request(
+                'POST',
+                'products',
+                [
+                'haders'=>['Content-Type' => 'application/vnd.api+json'],
+                'json' => $data
+                ]
+            );*/
 //add -- 结束
 
 //edit -- 开始
-// $data = array("data"=>array("type"=>"products",
-//                             "attributes"=>array(
-//                                                 "name"=>"name3333",
-//                                                 "price"=>100.1,
-//                                                 "content"=>array(
-//                                                             '产品内容'
-//                                                         ),
-//                                                )
-//                            )
-//               );
-// $response = $client->request(
-//                 'PUT',
-//                 'products/2',
-//                 [
-//                 'haders'=>['Content-Type' => 'application/vnd.api+json'],
-//                 'json' => $data
-//                 ]
-//             );
+/*$data = array("data"=>array("type"=>"products",
+                            "attributes"=>array(
+                                                "name"=>"name3333",
+                                                "price"=>100.1,
+                                                "content"=>array(
+                                                            '产品内容'
+                                                        ),
+                                               )
+                           )
+              );
+$response = $client->request(
+                'PUT',
+                'products/1',
+                [
+                'haders'=>['Content-Type' => 'application/vnd.api+json'],
+                'json' => $data
+                ]
+            );*/
+//edit -- 结束
 
-var_dump($response->getHeaders());
+//enable --开始
+$response = $client->request(
+                'PUT',
+                'products/1/enable',
+                [
+                'haders'=>['Content-Type' => 'application/vnd.api+json']
+                ]
+            );
+//enable --结束
+
+//disable --开始
+/*$response = $client->request(
+                'PUT',
+                'products/1/disable',
+                [
+                'haders'=>['Content-Type' => 'application/vnd.api+json']
+                ]
+            );*/
+//disable --结束
+
+
 $status = $response->getStatusCode();
 $body = $response->getBody();
 $contents = $body->getContents();
 
-//edit -- 结束
 
 echo "<pre>";
 
 print_r($status);
 print_r($contents);
+
+/*
+{
+    "meta": [],
+    "data": {
+        "type": "products",
+        "id": "1",
+        "attributes": {
+            "category": 2,
+            "name": "productName",
+            "price": 10.2,
+            "number": "123456",
+            "content": [
+                "\u4ea7\u54c1\u5185\u5bb9"
+            ],
+            "updateTime": 0,
+            "createTime": 1505291752,
+            "statusTime": 0,
+            "status": 0
+        },
+        "relationships": {
+            "userGroup": {
+                "data": {
+                    "type": "userGroups",
+                    "id": "10"
+                }
+            },
+            "department": {
+                "data": {
+                    "type": "departments",
+                    "id": "2"
+                }
+            }
+        },
+        "links": {
+            "self": "127.0.0.1\/products\/1"
+        }
+    }
+}
+*/
